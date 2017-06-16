5671056768ceffea9e35cedd7459b23abcf5a185
mark-ocampo <mark.ocampo@rea-group.com>
2017-06-16 14:54:37 +1000

    Whitelist http streeter-ui domain

--- api/config.js
+++ api/config.js
@@ -3,5 +3,10 @@ exports.appId = 'appid=dj0zaiZpPWpNQ1A1dk8xTXk0TyZzPWNvbnN1bWVyc2VjcmV0Jng9OTE-'
 exports.output = 'output=json';
 exports.callback = 'yj-4s244';
 
-const whitelist = ['https://streeter-ui.herokuapp.com', 'http://localhost:3000'];
+const whitelist = [
+  'http://streeter-ui.herokuapp.com',
+  'https://streeter-ui.herokuapp.com',
+  'http://localhost:3000'
+];
+
 exports.origin = { origin: whitelist };
